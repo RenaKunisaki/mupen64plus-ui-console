cb4d188afd9665721e768df0164a1755df863932
Rena Kunisaki <hyperhacker@gmail.com>
2015-02-09 02:03:53 -0500

    edit test script

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@ COREDIR=../mupen64plus-core
 CORELIB=$COREDIR/projects/unix/libmupen64plus.so.2
 OPTIONS="--resolution 640x480 --windowed --osd --rsp mupen64plus-rsp-hle \
 	--audio mupen64plus-audio-sdl"
-TESTSCRIPT=$COREDIR/data/test.lua
+#TESTSCRIPT=$COREDIR/data/test.lua
+TESTSCRIPT=$COREDIR/data/test-breakpoints.lua
 ROM="/home/rena/games/n64/Mario Kart 64 (USA).n64"
 
 ./projects/unix/mupen64plus \
